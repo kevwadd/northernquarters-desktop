de29a528e455aa1beac5c3286e3eeb065aa7aa60
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-06-07 14:54:22 +0100

    First commit - maintenance mode styled and ready

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="sr-only" href="#content">Skip to content</a>
+	
+	<header id="masthead" class="header-normal" role="banner">
+	
+	</header><!-- #masthead -->
+	
+	<main id="content" class="site-content">
