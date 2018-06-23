d45241147017bffc21f9c99722c90e0a0dd3c7f9
Kevin Waddell <kevwaddell@mac.com>
2018-06-23 14:05:25 +0100

    add container

--- parts/sections/section-location.php
+++ parts/sections/section-location.php
@@ -5,17 +5,17 @@ $directions_link = get_field('directions_link', 'options');
 <div class="rule no-mag-bot"></div>
 <section id="location-map" class="page-section">
 	<div class="container">
-		<h2 class="section-header no-mag-top"><span>Location</span></h2>
-		<?php if (!empty($event_address)) { ?>
-		<div class="address-box">
-			<address>
-			<?php echo $event_address; ?>
-			</address>
-			<?php if (!empty($directions_link)) { ?>
-			<a href="<?php echo $directions_link; ?>" class="btn btn-light btn-block" target="_blank">Directions<i class="fa fa-angle-right fa-lg"></i></a>
-			<?php } ?>
-		</div>
+	<h2 class="section-header no-mag-top"><span>Location</span></h2>
+	<?php if (!empty($event_address)) { ?>
+	<div class="address-box">
+		<address>
+		<?php echo $event_address; ?>
+		</address>
+		<?php if (!empty($directions_link)) { ?>
+		<a href="<?php echo $directions_link; ?>" class="btn btn-light btn-block" target="_blank">Directions<i class="fa fa-angle-right fa-lg"></i></a>
 		<?php } ?>
+	</div>
+	<?php } ?>
 	</div>
 	<div id="map">
 		<?php echo do_shortcode('[wpgmza id="1"]'); ?>
